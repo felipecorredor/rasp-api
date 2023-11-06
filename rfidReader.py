fb72cd91c89ff73841c97e999b0f5c863a9bb3dd
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-06 18:39:19 -0500

    rfid with semaphore

--- rfidReader.py
+++ rfidReader.py
@@ -1,9 +1,32 @@
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+from mfrc522 import MFRC522
+import time
+from semaforo import Semaforo
 
-rfid = SimpleMFRC522()
+lector = MFRC522(spi_id=0,sck=2,miso=4,mosi=3,cs=1,rst=0)
+
+semaforo1 = Semaforo(37,35,33,2,0)
+semaforo2 = Semaforo(3,5,7,2,0)
+
+TARJETA = 150564635253
+LLAVERO = 75147236
 
 while True:
-        id, text = rfid.read()
-        print(id)
-        print(text)
+    lector.init()
+    (stat, tag_type) = lector.request(lector.REQIDL)
+    if stat == lector.OK:
+        (stat, uid) = lector.SelectTagSN()
+        if stat == lector.OK:
+            identificador = int.from_bytes(bytes(uid),"little",False)
+            
+            if identificador == TARJETA:
+                print("UID: "+ str(identificador)+" Acceso concedido")
+                semaforo1.state = 2
+                semaforo2.state = 2
+                
+            elif identificador == LLAVERO:
+               print("UID: "+ str(identificador)+" Acceso concedido")
+               semaforo1.state = 1
+               semaforo2.state = 1
+                
+            else:
+                print("UID: "+ str(identificador)+" desconocido: Acceso denegado")
