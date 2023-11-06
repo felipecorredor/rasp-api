62fd7876b6f63847be7139479e783f7debff008e
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-06 18:50:52 -0500

    update rfidReader

--- rfidReader.py
+++ rfidReader.py
@@ -1,32 +1,33 @@
-from mfrc522 import MFRC522
-import time
-from semaforo import Semaforo
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
-lector = MFRC522(spi_id=0,sck=2,miso=4,mosi=3,cs=1,rst=0)
+semaforo1 = Semaforo(37, 35, 33, 2, 0)
+semaforo2 = Semaforo(3, 5, 7, 2, 0)
 
-semaforo1 = Semaforo(37,35,33,2,0)
-semaforo2 = Semaforo(3,5,7,2,0)
+rfid = SimpleMFRC522()
 
 TARJETA = 150564635253
 LLAVERO = 75147236
 
+def relay_on(channel):
+    semaforo1.state = channel
+    semaforo2.state = channel
+
+def relay_off(channel):
+    semaforo1.state = channel
+    semaforo2.state = channel
+
 while True:
-    lector.init()
-    (stat, tag_type) = lector.request(lector.REQIDL)
-    if stat == lector.OK:
-        (stat, uid) = lector.SelectTagSN()
-        if stat == lector.OK:
-            identificador = int.from_bytes(bytes(uid),"little",False)
-            
-            if identificador == TARJETA:
-                print("UID: "+ str(identificador)+" Acceso concedido")
-                semaforo1.state = 2
-                semaforo2.state = 2
-                
-            elif identificador == LLAVERO:
-               print("UID: "+ str(identificador)+" Acceso concedido")
-               semaforo1.state = 1
-               semaforo2.state = 1
-                
-            else:
-                print("UID: "+ str(identificador)+" desconocido: Acceso denegado")
+    id, text = rfid.read()
+    print(id)
+    
+    if id == TARJETA:
+        relay_on(2)  
+        print(text + ": Access granted")
+    
+    elif id == LLAVERO:
+        relay_on(1) 
+        print(text + ": Access granted")
+    
+    else:
+        print("Not allowed...")
