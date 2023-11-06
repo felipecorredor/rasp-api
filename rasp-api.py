7656e3706dbc18826113673f8e1fd5381b242405
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-06 18:16:00 -0500

    init with rfid

--- rasp-api.py
+++ rasp-api.py
@@ -1,81 +1,81 @@
 from flask import request, jsonify
 from flask_api import FlaskAPI
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+import threading
+from semaforo import Semaforo
+from semaforo_esquina import Semaforo_esquina
 import threading
 import time
-import asyncio
-import websockets
-from tarjetrfid import TarjetaRFID  # Asegúrate de que esta importación sea correcta
-import json
 
-# Otras importaciones y configuraciones aquí...
+#freq1=1
+semaforo1 = Semaforo(37,35,33,2,0)
+semaforo2 = Semaforo(3,5,7,2,0)
+semaforo3 = Semaforo_esquina(37,35,33,3,5,7,2,0)
 
 app = FlaskAPI(__name__)
 CORS(app)
 
-# Otras configuraciones aquí...
+# Configura la clave secreta para JWT
+app.config['JWT_SECRET_KEY'] = '3zsjMDUSBk2M9@'
+jwt = JWTManager(app)
 
-id = TarjetaRFID()  # Inicializa la instancia del lector RFID
+thsem1 = threading.Thread(target=semaforo1.paint)
+thsem1.start()
 
-# Función para enviar lectura RFID al WebSocket
-async def enviar_lectura_al_websocket():
-    websocket_url = "wss://qti41egldh.execute-api.us-east-1.amazonaws.com/production"
-    message = {"action": "sendmessage", "message": "hello, everyone!"}
-    message_json = json.dumps(message)
+thsem2 = threading.Thread(target=semaforo2.paint)
+thsem2.start()
 
-    async with websockets.connect(websocket_url) as websocket:
-        while True:
-            rfid_value = id.read_id()
-            print("enviar_lectura_al_websocket")
-            await websocket.send(message_json)
+thsem3 = threading.Thread(target=semaforo3.paint)
+thsem3.start()
 
-            if rfid_value:
-                # Enviar la lectura al WebSocket
-                await websocket.send(message)
-            await asyncio.sleep(1)
 
-def rfiid_loop(id):
-    while True:
-        rfid_value = id.read_id()
-        if rfid_value == 150564635253:
-            if semaforo1.state != 2:
-                semaforo1.state = 2
-                semaforo2.state = 2
-            else:
-                semaforo1.state = 0
-                semaforo2.state = 0
-            print({"success": "Es tarjeta válida"})
-            
-            # Inicia el envío al WebSocket en un hilo separado
-            print("rfiid_loop")
-            asyncio.create_task(enviar_lectura_al_websocket())
+# Ejemplo de autenticación con usuario y contraseña
+@app.route('/login', methods=["POST"])
+def login():
+    username = request.json.get("username")
+    password = request.json.get("password")
+    
+    if username == "user" and password == "password":
+        # Genera un token JWT válido
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"error": "Credenciales inválidas"}), 401
 
-        elif rfid_value == 214018868130:
-            if semaforo1.state != 2:
-                semaforo1.state = 2
-                semaforo2.state = 2
-            else:
-                semaforo1.state = 0
-                semaforo2.state = 0
-            print({"success": "Es llavero válido"})
-
-            # Inicia el envío al WebSocket en un hilo separado
-            rfiid_loop("rfiid_loop::else")
-            asyncio.create_task(enviar_lectura_al_websocket())
-
+# Ejemplo de una ruta protegida con autenticación JWT
+@app.route('/set_state', methods=["POST"])
+@jwt_required()
+def set_state():
+    if request.method == "POST":
+        new_state = request.data.get("state")
+        if new_state is not None and new_state in [0,1,2,3]:
+            semaforo1.state = new_state
+            semaforo2.state = new_state
+            semaforo3.state = new_state
+            return {"message": f"Estado cambiado a {new_state}", "state1": semaforo1.state, "state2": semaforo2.state, "state3": semaforo3.state }
         else:
-            print({"error": "Intento inválido de tarjeta o llavero"})
-        time.sleep(1)
+            return {"error": "Estado no válido"}
 
-# Rutas y otras funciones aquí...
-
-# Resto del código...
+@app.route('/set_freq', methods=["GET","POST"])
+@jwt_required()
+def set_freq():
+    if request.method == "POST":
+        new_freq = int(request.data.get("freq"))
+        semaforo = int(request.data.get("semaforo"))
+        if new_freq is not None and semaforo in [1,2,3]:
+            global freq1  # Asigna freq1 como global
+            freq1 = new_freq
+            if semaforo == 1:
+                semaforo1.freq = new_freq
+            elif semaforo == 2:
+                semaforo2.freq = new_freq
+            elif semaforo == 3:
+                semaforo3.freq = new_freq
+            return {"message": f"Frecuencia cambiada a {new_freq}", "freq1": freq1, "semaforo": semaforo}
+        else:
+            return {"error": "Frecuencia o semáforo no válido"}
 
 if __name__ == "__main__":
-    # Hilo para la lectura de tarjetas RFID
-    thread_rfiid = threading.Thread(target=rfiid_loop, args=(id,))
-    thread_rfiid.start()
 
-    print("this is: __main__")
     app.run()
