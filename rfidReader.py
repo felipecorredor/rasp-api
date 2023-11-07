8a602a21acf041b8615810babb1ff6e0c4b97480
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-06 19:24:36 -0500

    try bucle semaphore

--- rfidReader.py
+++ rfidReader.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from semaforo import Semaforo
+import threading
 
 semaforo1 = Semaforo(37, 35, 33, 2, 0)
 semaforo2 = Semaforo(3, 5, 7, 2, 0)
@@ -13,14 +14,26 @@ LLAVERO = 214018868130
 def relay_on(channel):
     semaforo1.state = channel
     semaforo2.state = channel
-    semaforo1.paint()
-    semaforo2.paint()
 
 def relay_off(channel):
     semaforo1.state = channel
     semaforo2.state = channel
-    semaforo1.paint()
-    semaforo2.paint()
+
+# Función para ejecutar semaforo1.paint() en un hilo separado
+def semaforo1_thread():
+    while True:
+        semaforo1.paint()
+
+# Función para ejecutar semaforo2.paint() en un hilo separado
+def semaforo2_thread():
+    while True:
+        semaforo2.paint()
+
+# Inicia los hilos para las funciones de los semáforos
+thread1 = threading.Thread(target=semaforo1_thread)
+thread2 = threading.Thread(target=semaforo2_thread)
+thread1.start()
+thread2.start()
 
 while True:
     id, text = rfid.read()
@@ -36,4 +49,3 @@ while True:
     
     else:
         print("Not allowed")
-    
