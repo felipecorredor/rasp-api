ad36d998cf2b1dc4bf794ed45c9190cb72fc70e1
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-06 19:28:28 -0500

    update threadings

--- rfidReader.py
+++ rfidReader.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from semaforo import Semaforo
 import threading
+import queue
 
 semaforo1 = Semaforo(37, 35, 33, 2, 0)
 semaforo2 = Semaforo(3, 5, 7, 2, 0)
@@ -19,15 +20,20 @@ def relay_off(channel):
     semaforo1.state = channel
     semaforo2.state = channel
 
-# Función para ejecutar semaforo1.paint() en un hilo separado
+# Cola para coordinar las actualizaciones de semáforos
+semaforo_queue = queue.Queue()
+
+# Función para ejecutar semaforo1.paint()
 def semaforo1_thread():
     while True:
         semaforo1.paint()
+        semaforo_queue.put("Semaforo1")
 
-# Función para ejecutar semaforo2.paint() en un hilo separado
+# Función para ejecutar semaforo2.paint()
 def semaforo2_thread():
     while True:
         semaforo2.paint()
+        semaforo_queue.put("Semaforo2")
 
 # Inicia los hilos para las funciones de los semáforos
 thread1 = threading.Thread(target=semaforo1_thread)
@@ -49,3 +55,11 @@ while True:
     
     else:
         print("Not allowed")
+    
+    # Procesa las actualizaciones de semáforos de la cola
+    try:
+        while True:
+            updated_semaforo = semaforo_queue.get_nowait()
+            print(f"Actualizado: {updated_semaforo}")
+    except queue.Empty:
+        pass
