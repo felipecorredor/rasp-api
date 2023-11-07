95d34d01972da8fe6e9b8beb28e71a5b3c80f8e3
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-06 21:08:14 -0500

    update control_semaforo

--- rfidReader.py
+++ rfidReader.py
@@ -40,13 +40,20 @@ def read_rfid():
         else:
             print("Not allowed")
 
-def control_semaforos():
+def control_semaforo1():
     while True:
-        semaforo2.paint()
         semaforo1.paint()
 
+def control_semaforo2():
+    while True:
+        semaforo2.paint()
+        
+
 thread_rfid = threading.Thread(target=read_rfid)
-thread_semaforos = threading.Thread(target=control_semaforos)
+thread_semaforo1 = threading.Thread(target=control_semaforo1)
+thread_semaforo2 = threading.Thread(target=control_semaforo2)
 
 thread_rfid.start()
-thread_semaforos.start()
+
+thread_semaforo1.start()
+thread_semaforo2.start()
