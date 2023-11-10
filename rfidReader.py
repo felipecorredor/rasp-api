eec85fd892d4c48dd22b6f2bd2067545be464385
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-09 22:19:53 -0500

    update

--- rfidReader.py
+++ rfidReader.py
@@ -1,27 +1,57 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import signal
-import sys
+from semaforo import Semaforo
+import threading
+import websocket
+import json
 
-reader = SimpleMFRC522()
-is_reading = True
+semaforo1 = Semaforo(37, 35, 33, 2, 0)
+semaforo2 = Semaforo(3, 5, 7, 2, 0)
+rfid = SimpleMFRC522()
+TARJETA = 150564635253
+LLAVERO = 214018868130
 
-GPIO.setwarnings(False)
+websocket_url = "wss://qti41egldh.execute-api.us-east-1.amazonaws.com/production"
 
-# Capture SIGINT for cleanup
-def end_read(signal, frame):
-    global is_reading
-    print('Ctrl+C captured, exiting')
-    is_reading = False
-    sys.exit()
+def relay_on(channel):
+    semaforo1.state = channel
+    semaforo2.state = channel
 
-# Hook the SIGINT
-signal.signal(signal.SIGINT, end_read)
+    # websocket
+    ws = websocket.create_connection(websocket_url)
+    message = {"action": "sendmessage", "message": "websocket connection"}
+    ws.send(json.dumps(message))
+    ws.close()
 
-while is_reading:
-    try:
-        id, text = reader.read()
-        print(f'ID :: {id}')
-        print(f'Badge Number :: {text}')
-    finally:
-        GPIO.cleanup()
+def relay_off(channel):
+    semaforo1.state = channel
+    semaforo2.state = channel
+
+def read_rfid():
+    while True:
+        id, text = rfid.read()
+        print(id)
+        if id == TARJETA:
+            relay_on(2)  
+            print(text + ": Access granted")
+        elif id == LLAVERO:
+            relay_on(1) 
+            print(text + ": Access granted")
+        else:
+            print("Not allowed")
+
+def control_semaforo_uno():
+    while True:
+        semaforo1.paint()
+
+# def control_semaforo_dos():
+#     while True:
+#         semaforo2.paint()
+
+thread_rfid = threading.Thread(target=read_rfid)
+thread_semaforo_uno = threading.Thread(target=control_semaforo_uno)
+# thread_semaforo_dos = threading.Thread(target=control_semaforo_dos)
+
+thread_rfid.start()
+thread_semaforo_uno.start()
+# thread_semaforo_dos.start()
