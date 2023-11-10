8bf323b14343ae76e53b2d09cdd3ef623a7f2cb0
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-09 22:11:34 -0500

    Functional

--- rfidReader.py
+++ rfidReader.py
@@ -7,41 +7,31 @@ import json
 import signal
 import sys
 
-# Suppress GPIO warnings
-GPIO.setwarnings(False)
-
-# Initialize traffic lights
 semaforo1 = Semaforo(37, 35, 33, 2, 0)
 semaforo2 = Semaforo(3, 5, 7, 2, 0)
-
-# Initialize RFID reader
 reader = SimpleMFRC522()
 is_reading = True
 
-# GPIO setup
 GPIO.setwarnings(False)
 
-# RFID card IDs
 TARJETA = 150564635253
 LLAVERO = 214018868130
 
-# Websocket setup
 websocket_url = "wss://qti41egldh.execute-api.us-east-1.amazonaws.com/production"
 
-# Function to handle turning on the relays and updating traffic light states
 def relay_on(channel):
-    # semaforo1.state = channel
-    # semaforo2.state = channel
+    semaforo1.state = channel
+    semaforo2.state = channel
 
-    # Update the traffic lights
-    # semaforo1.paint()
-    # semaforo2.paint()
-
-    # WebSocket
+    # websocket
     ws = websocket.create_connection(websocket_url)
     message = {"action": "sendmessage", "message": "websocket connection"}
     ws.send(json.dumps(message))
     ws.close()
+
+def relay_off(channel):
+    semaforo1.state = channel
+    semaforo2.state = channel
 
 # Capture SIGINT for cleanup
 def end_read(signal, frame):
@@ -57,13 +47,12 @@ while is_reading:
     try:
         id, text = reader.read()
         if id == TARJETA:
+            relay_on(2)  
             print(text + ": Access granted")
-            # relay_on(2)  
         elif id == LLAVERO:
+            relay_on(1) 
             print(text + ": Access granted")
-            # relay_on(1) 
         else:
             print("Not allowed")
     finally:
-        # Avoid GPIO cleanup inside the loop
-        pass
+        GPIO.cleanup()
