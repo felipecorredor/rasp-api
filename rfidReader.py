d368fa854c032df6eb06b87ba94e3a6f029bc200
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-09 22:05:40 -0500

    send websocket

--- rfidReader.py
+++ rfidReader.py
@@ -30,18 +30,18 @@ websocket_url = "wss://qti41egldh.execute-api.us-east-1.amazonaws.com/production
 
 # Function to handle turning on the relays and updating traffic light states
 def relay_on(channel):
-    semaforo1.state = channel
-    semaforo2.state = channel
+    # semaforo1.state = channel
+    # semaforo2.state = channel
 
     # Update the traffic lights
-    semaforo1.paint()
-    semaforo2.paint()
+    # semaforo1.paint()
+    # semaforo2.paint()
 
     # WebSocket
-    # ws = websocket.create_connection(websocket_url)
-    # message = {"action": "sendmessage", "message": "websocket connection"}
-    # ws.send(json.dumps(message))
-    # ws.close()
+    ws = websocket.create_connection(websocket_url)
+    message = {"action": "sendmessage", "message": "websocket connection"}
+    ws.send(json.dumps(message))
+    ws.close()
 
 # Capture SIGINT for cleanup
 def end_read(signal, frame):
